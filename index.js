c448add65d04a87d1c4fd1b40afb1357a75fb489
saurabh kumar <35069331+saurabhpojhi@users.noreply.github.com>
2021-10-15 00:04:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const express = require('express')
+
+const express = require("express");
 const path = require("path");
-const app = express()
+// const bodyParser = require("body-parser");
+// const router = require("./UserRoutes");
+
+const app = express();
 const port = 4000;
 
-app.get('/', (req, res) =>{
-    console.log(path.join(__dirname + "./index.html"));
-    res.sendFile(path.join(__dirname + "./index.html"));
-})
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use("/api/v1", router);
 
-app.get('/about', (req, res) =>{
-    res.send(' about')
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 
-app.post("/", (req,res)=>{
-    const userName= req.body.name;
-})
-
-app.listen(port, ()=>{
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+app.post("/api/v1/login", (req, res) => {
+    res.sendFile(" <h1> Done </h1>");
+    console.log("name");
+  });
+  
+app.listen(port, () => {
+  console.log(`Server is Woring on port: ${port}`);
+});
