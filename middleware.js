9abcfca3a9f0e9f8e28734c7a1a57ca4f8507d89
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-01 11:31:31 +0530

    add

--- middleware.js
+++ middleware.js
@@ -1,10 +1,35 @@
 // middle ware functions  exmaple 
 
- var express = require("express");
- var app = express();
+//  var express = require("express");
+//  var app = express();
 
- app.get('/',function(req,res,next){
-     next();
+//  app.get('/',function(req,res,next){
+//      next();
+//  })
+
+//  app.listen(3000);
+
+const express = require("express");
+ const app = express();
+
+app.use('/abc', express.static('public'));
+
+// mylogger means middleware  
+
+var mylogger = function(req,res,next){
+    console.log("middelware working");
+    next();
+}
+
+app.use(mylogger); // middelware define 
+
+ app.get('/',function(req,res){
+     res.send("hello middleware")
+     
  })
+ app.get('/users',function(req,res){
+    res.send(" users hello middleware")
+    
+})
 
  app.listen(3000);
