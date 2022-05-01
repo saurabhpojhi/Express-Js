e5e900bd3253da7959a72e83a089ebf81326a205
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-01 11:12:26 +0530

    add

--- main.js
+++ main.js
@@ -9,9 +9,25 @@ app.get("/users",(req,res)=>{
     res.send("user data acced");
 })
 
-// app.post("/users/profile",(req,res)=>{
-//     res.send("user data acced");
+// app.get("/users/:Id",(req,res)=>{
+//    // console.log(req.params);
+//    if(req.params.Id == undefined){
+//     res.send(" all user data acced");
+//    }
+//    else
+//    res.send("user data acced"+req.params.Id);
 // })
+
+app.get("/flights/:From-:To",(req,res)=>{
+     console.log(req.params);
+    
+    res.send("search flight "+ "From:" + req.params.From+" To"+req.params.To);
+ })
+
+
+app.post("/users/profile",(req,res)=>{
+    res.send("user data acced");
+})
 
 app.listen(port, ()=>{
     console.log(`Example app listening at http://localhost:${port}`)
